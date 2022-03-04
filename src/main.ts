5a4bf1602209c0cacd9d0b27e17515103a1a0a68
Piotr <piotr.ostrowski@aol.com>
2022-03-04 02:20:51 +0000

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from "@nestjs/core";
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
 import { ApplicationModule } from "./app.module";
 
 async function bootstrap() {
-  const app = await NestFactory.create(ApplicationModule, { cors: true });
+  const app = await NestFactory.create(ApplicationModule);
+
+  const config = new DocumentBuilder()
+    .setTitle("Plug API")
+    .setDescription("Plug got proxies and tokens")
+    .setVersion("1.0")
+    .addTag("plug")
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("api", app, document);
+
   await app.listen(3000);
-  console.log(`Application is running on: ${await app.getUrl()}`);
 }
+
 bootstrap();
