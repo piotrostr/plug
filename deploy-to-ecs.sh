3c9f80c34774b465289ccb0156e170b58e0b744d
Piotr <piotr.ostrowski@aol.com>
2022-03-10 11:37:06 +0100

    deploy to ecs successfully

--- deploy-to-ecs.sh
+++ deploy-to-ecs.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 docker context use default && \
-  docker build -t piotrostr/plug . && \
-  docker push piotrostr/plug && \
+  docker compose build && \
+  docker compose push && \
   docker context use plug-ecs-context && \
-  docker compose up && \
-  docker context use default
+  docker compose up
