27cbe8c35562208aca2ce6014049d67b7b91081c
Piotr <piotr.ostrowski@aol.com>
2022-03-13 12:22:19 +0100

    update mutation types

--- src/user/user.mutations.ts
+++ src/user/user.mutations.ts
@@ -2,15 +2,36 @@ import { InputType, Field, PartialType } from "@nestjs/graphql";
 
 @InputType()
 export class CreateUserInput {
-  @Field(() => String, { description: "User" })
-  user: string;
+  @Field(() => String)
+  _id: string;
+
+  @Field(() => String)
+  token: string;
+
+  @Field(() => String)
+  email: string;
+
+  @Field(() => String)
+  username: string;
+
+  @Field(() => String)
+  password: string;
+
+  @Field(() => String)
+  country: string;
+
+  @Field(() => Number)
+  dayOfBirth: number;
+
+  @Field(() => Number)
+  monthOfBirth: number;
+
+  @Field(() => Number)
+  yearOfBirth: number;
 }
 
 @InputType()
 export class UpdateUserInput extends PartialType(CreateUserInput) {
-  @Field(() => String)
-  _id: string;
-
   @Field(() => Boolean)
   isCurrentlyUsed: boolean;
 
