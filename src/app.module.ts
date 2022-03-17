5686fab319b9f3539020d4146a88e06e33a409d5
Piotr <piotr.ostrowski@aol.com>
2022-03-17 17:41:55 +0300

    enable mongodb authentication

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,11 +8,14 @@ import { ConfigModule } from "@nestjs/config";
 import { AuthModule } from "./auth/auth.module";
 
 const host = process.env.MONGO_HOST || "localhost";
+const username = process.env.MONGO_INITDB_ROOT_USERNAME;
+const password = process.env.MONGO_INITDB_ROOT_PASSWORD;
+const uri = `mongodb://${username}:${password}@${host}:27017/db`;
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    MongooseModule.forRoot(`mongodb://${host}:27017/db`),
+    MongooseModule.forRoot(uri),
     UserModule,
     ProxyModule,
     AuthModule,
