b97bba7c39dcd8bff5c2ad7023a9453e7cbff8d5
Piotr <piotr.ostrowski@aol.com>
2022-03-17 21:49:02 +0300

    remove script, throws weird err

--- deploy-to-ecs.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-docker context use default && \
-  docker compose --file ecs-compose.yml build && \
-  docker compose --file ecs-compose.yml push && \
-  docker context use plug-ecs-context && \
-  docker compose --file ecs-compose.yml up
